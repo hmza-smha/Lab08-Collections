f07fc650cae19dba1fc03a9c867d0ed98a86b93a
MutazTB <mutazaltbakhi900@gmail.com>
2022-04-06 01:58:31 +0300

    Library

--- Lab08-Collections/Program.cs
+++ Lab08-Collections/Program.cs
@@ -6,28 +6,29 @@ namespace Lab08_Collections
     {
         static void Main(string[] args)
         {
-            Library lib = new Library();
+            Library<Book> lib = new Library<Book>();
 
             lib.Add("1", "HamZa", "Akram", 5);
-            lib.Add("2", "Hello", "World", 10);
-
+            lib.Add("2", "Mutaz", "Altbakhi", 10);
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Count: " + lib.Count);
-
-            Console.WriteLine("My Books");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("My Books :");
             foreach (Book book in lib)
             {
                 Console.WriteLine(book.Author);
             }
-
+            Console.WriteLine();
             Backpack<Book> bp = new Backpack<Book>();
             bp.Pack(lib.Borrow("2"));
-
-            Console.WriteLine("My Pack");
-            foreach (Book book in bp)
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("My Pack : ");
+            foreach (var book in bp)
             {
-                Console.WriteLine(book.Title);
+                Console.WriteLine(book.Author);
             }
-
+            Console.ForegroundColor = ConsoleColor.Black;
 
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Lab08-Collections-1
+# Lab08-Collections 
+
+## Descreption
+
+This is a C# console app that uses multiple classes and a generic collection to create a basic lending library with functionality for adding and removing books from the library.
+
+## Visual 
+
+Run 
+![image](Images/LibraryRun.png)
+
+---
+
+Test 
+![image](Images/LibraryTest.png)
